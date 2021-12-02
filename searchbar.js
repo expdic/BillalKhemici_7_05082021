2d513e73f8806ec655a2691b1a628bc2a352b8e2
expdic <89906405+expdic@users.noreply.github.com>
2021-12-02 17:12:46 +0100

    Commit algo 1

--- searchbar.js
+++ searchbar.js
@@ -1,43 +1,44 @@
-const searchBar = document.getElementById('searchbar')
-let recipearemp = []
+const searchBar = document.getElementById('searchbar') // récuperer la barre de recherche 
+let recipearemp = [] // mon tableau
+document.getElementById("submit").disabled = 'true'; //désactiver le submit 
 
-document.getElementById("submit").disabled = 'true';
+searchBar.addEventListener('keyup', (e) => { // quand l'utilisateur commence à ecrire 
+    charrech = e.target.value.toLowerCase(); // récuperer ce qu'il ecrit et le mettre en miniscule 
 
-searchBar.addEventListener('keyup', (e) => {
-    charrech = e.target.value.toLowerCase();
-    if ( charrech.length > 2 ) {
-        for (var i = 0; i < recipes.length; i++) {
-            for (var k = 0; k < recipes[i].ingredients.length; k++) {
-                if (recipes[i].ingredients[k].ingredient.toLowerCase().includes(charrech) == true && recipearemp.includes(recipes[i]) == false) {
-                    recipearemp.push(recipes[i]);
-                }
+    if (charrech.length < 2) { // si il écrit plus de 2 caracteres 
+        main.innerHTML = "" // vider mon main 
+        filtres.innerHTML = "" // vider les filtres 
+        recipearemp = [] // vider mon tableau 
+        verifrecipes(recipes) // afficher toutes mes recettes 
+        showapp(recipes) // afficher tout les appareils
+        return;
+    }
+    for (var i = 0; i < recipes.length; i++) { // pour chaque recette 
 
-                for (var j = 0; j < recipearemp.length; j++) {
-                    if (recipearemp[j].name.toLowerCase().includes(charrech) == false && recipearemp[j].description.toLowerCase().includes(charrech) == false && recipearemp[j].ingredients[k].ingredient.toLowerCase().includes(charrech) == false ) {
-                        recipearemp.splice(j, 1);
-                    }
-                }
+        for (var k = 0; k < recipes[i].ingredients.length; k++) { // pour chaque ingrédient de la recette 
 
-            }
-            
-            if (recipes[i].name.toLowerCase().includes(charrech) == true && recipearemp.includes(recipes[i]) == false || recipes[i].description.toLowerCase().includes(charrech) == true && recipearemp.includes(recipes[i]) == false ) {
-                recipearemp.push(recipes[i]);
+            if (recipes[i].ingredients[k].ingredient.toLowerCase().includes(charrech) == true && // si l'ingrédient contient ce que l'utilisateur écrit et que l'ingrédient n'est pas dans mon tableau
+                recipearemp.includes(recipes[i]) == false) {
+                recipearemp.push(recipes[i]); // ajoute la recette dans mon tableau 
             }
 
-          
-
+            var recipearemp = Array.from(new Set(recipearemp)); // supprimer les doublons 
         }
-        main.innerHTML = "";
-        verifrecipes(recipearemp);
+
+        if (recipes[i].name.toLowerCase().includes(charrech) == true && recipearemp.includes(recipes[i]) == false || // si le nom de ma recette contient ce que l'utilisateurr écrit et que ma recette et pas dans mon tableau 
+            recipes[i].description.toLowerCase().includes(charrech) == true && recipearemp.includes(recipes[i]) == false) { // ou si la descriptopn de ma recette contient ce que l'utilisateurr écrit et que ma recette et pas dans mon tableau 
+            recipearemp.push(recipes[i]); // ajouter ma recette au tableauu 
+        }
     }
 
-    else {
-        main.innerHTML = ""
-        recipearemp = []
-        verifrecipes(recipes)
-    }
+    main.innerHTML = ""; // vider le main ou les recette vont s'afficher 
+    filtres.innerHTML = "" // vider les filtres ou les appreils, ustensils, ingrédient de mes recettes vont s'afficehr 
+    verifrecipes(recipearemp); // vérifier si mon tableau n'est pas vide et afficher les recettes 
+    showapp(recipearemp) // afficher les appareils 
 
-   
+
+    
 })
 
-console.log(recipes[3].ingredients[3].ingredient.toLowerCase().includes('oeuf  '))
+
+    
