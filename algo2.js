6765f1b14913addd1757d3f7752b25977544a997
expdic <89906405+expdic@users.noreply.github.com>
2021-12-02 17:10:58 +0100

    Commit algo 2

---
+++ algo2.js
@@ -0,0 +1,45 @@
+const searchBar = document.getElementById('searchbar') 
+let recipearemp = []
+document.getElementById("submit").disabled = 'true'; 
+searchBar.addEventListener('keyup', (e) => {  
+    const charrech = e.target.value.toLowerCase();
+    if (charrech.length > 2) {
+        var rech = research(charrech)
+        displayResult(rech) 
+    }
+
+    else {
+        displayResult()
+    }
+})
+
+function research(charrech) {
+    const result = recipes.filter(recette => {
+        return recette.name.toLowerCase().includes(charrech)
+            || recette.description.toLowerCase().includes(charrech)
+            || recette.appliance.toLowerCase().includes(charrech)
+            || recette.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(charrech))
+            || recette.ustensils.some((ustensil) => ustensil.toLowerCase().includes(charrech));
+
+    });
+    return result 
+    
+    
+}
+
+
+function displayResult(result) {
+    let filtres = document.querySelector(".appul");
+    filtres.innerHTML = ""
+    filtres2.innerHTML = ""
+    main.innerHTML = ""
+    let data = result ? result : recipes;
+    verifrecipes(data);
+    showapp(data)
+    showusten(data)
+    monapp2()
+    moning2()
+    monusten2()
+
+}
+
