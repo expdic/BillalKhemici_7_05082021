c1615b5ffbb363f8b667ccb9a8566b653ffea4d3
expdic <89906405+expdic@users.noreply.github.com>
2021-12-04 22:37:39 +0100

    delete fichier

--- showrecipes.js
+++ showrecipes.js
@@ -65,3 +65,23 @@ function showrecipes(recipvar) {
     }
 
 } // cette fonction a pour but d'afficher les recettes et les mettres dans des balises html 
+
+
+function verifrecipes(recipa) {
+
+    if (recipa.length == 0) { // si mon tableau est vide 
+        var divnoresult = document.createElement('div');
+        divnoresult.className = 'divnoresult';
+        var pnoresult = document.createElement('p');
+        pnoresult.className = 'pnoresult';
+        pnoresult.textContent = ' Veuillez renouveler vos recherches car nous n avons rien trouvé '
+        divnoresult.appendChild(pnoresult);
+        main.appendChild(divnoresult); // AFFICHER qu'il n'y pas de résultats 
+    }
+    
+    else (
+        showrecipes(recipa) // SINON, afficher les résultats .
+    )
+}
+
+verifrecipes(recipes) 
